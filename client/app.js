bcd5c0e0f8541449674f55a5babc7f65bfd26425
Murph Grainger <murph.grainger@gmail.com>
2016-12-04 20:32:19 -0700

    random exercise appears, still need to replace exercise on click

--- client/app.js
+++ client/app.js
@@ -1,25 +1,16 @@
 $(document).ready(function() {
     $.get('http://localhost:3000/abWorkouts', function(data) {
-        $('#get-workout').click(function() {
-            $paragraph = $('#paragraph')
-            let name = `<h4>${data[0].name}</h4>`;
-            let description = `<li>${data[0].description}</li>`;
-            let repetitions = `<li> Reps: ${data[0].repetitions}</li>`;
-            let sets = `<li> Sets: ${data[0].sets}</li>`;
-            $paragraph.append(name)
-            $paragraph.append(description)
-            $paragraph.append(repetitions)
-            $paragraph.append(sets)
+        $('#get-workout').on('click', function() {
+            let randomIndex = Math.floor(Math.random() * data.length)
+            let name = `<h4>${data[randomIndex].name}</h4>`;
+            let description = `<li>${data[randomIndex].description}</li>`;
+            let repetitions = `<li> Reps: ${data[randomIndex].repetitions}</li>`;
+            let sets = `<li> Sets: ${data[randomIndex].sets}</li>`;
+            $paragraph = $('#paragraph');
+            $paragraph.append(name);
+            $paragraph.append(repetitions);
+            $paragraph.append(sets);
+            $paragraph.append(description);
         });
-
-
-    })
-})
-
-function showWorkout(data) {
-    console.log(data);
-}
-//
-// $('#get-workout').click(function()
-//
-// );
+    });
+});
